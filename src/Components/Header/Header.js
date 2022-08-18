71e458f6e952396288d27695699c741ff13b426f
Ndumiso Thela <ndumisothela480@gmail.com>
2022-08-18 21:34:12 +0200

    remove head picture

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,9 +9,6 @@ const Header = () => {
         <h1 className="header_name">Sipho Nhlapo</h1>
         <h3 className="header_title">Fund specialist | Asset management</h3>
       </div>
-      <div>
-        <img className="profile_picture" src={siphoN} alt="profile_image" />
-      </div>
     </div>
   );
 };
